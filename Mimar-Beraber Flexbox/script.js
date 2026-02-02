c479690f9ec46ae56245d4e5ce9c577c221672ce
Sezer Ünalmış <76732537+Sezer5@users.noreply.github.com>
2026-02-02 11:24:55 +0300

    Anmiasyonlu mobil menü bitti

--- Mimar-Beraber Flexbox/script.js
+++ Mimar-Beraber Flexbox/script.js
@@ -2,9 +2,12 @@ const hamburger = document.querySelector(".hamburger");
 const bar1 = document.querySelector(".bar1");
 const bar2 = document.querySelector(".bar2");
 const bar3 = document.querySelector(".bar3");
+const mobilNav = document.querySelector(".mobileNav");
 
 hamburger.addEventListener("click",()=>{
-    bar1.classList.toggle("animationBar1")
-    bar2.classList.toggle("animationBar2")
-    bar3.classList.toggle("animationBar3")
+    bar1.classList.toggle("animationBar1");
+    bar2.classList.toggle("animationBar2");
+    bar3.classList.toggle("animationBar3");
+    mobilNav.classList.toggle("openSidebar");
+    
 })
